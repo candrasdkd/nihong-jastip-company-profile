10d30f7d13255a029a39f6b6f4bd211448e9d4d4
Candra Sidik Dermawan <86487468+candrasdkd@users.noreply.github.com>
2026-04-21 08:53:45 +0700

    fix: ensure NODE_ENV is set to test during jest execution to prevent environment conflicts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
+(process.env as any).NODE_ENV = 'test'
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
